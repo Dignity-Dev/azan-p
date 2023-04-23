bc7cfa2e7218edc8f1ceba710016a8c4c517816a
devfaoed <bashirhaadiy@gmail.com>
2023-04-23 11:13:15 +0100

    update

--- lovethetruth/admin/contact.php
+++ lovethetruth/admin/contact.php
@@ -33,8 +33,67 @@ include "nav.php";
                     <a href="article.php" class="btn btn-purple"><i class="fa fa-plus"></i> Create New Article</a>
                 </div>
             </div><!-- az-dashboard-one-title -->
+            <hr>
 
+            <!-- MAIN -->
+            <div class="row row-sm mg-b-20">
 
+                <div class="col-lg-10 offset-lg-1 mg-t-20 mg-lg-t-0">
+                    <div class="card">
+                        <div class="card-header">
+                            <div>
+                                <h6 class="card-title">All Messages</h6>
+                                <p class="card-text">Manage All Articles, Update and delete.</p>
+                            </div>
+
+                        </div><!-- card-header -->
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="myTable">
+                                    <thead>
+                                        <tr>
+                                            <th>S/N</th>
+                                            <th>Subject</th>
+                                            <th>Sender Name</th>
+                                            <th>Sender Email</th>
+                                            <th>Date Sent</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $counter = 1;
+                                        $fetch = mysqli_query($con, "SELECT * FROM message order by m_date DESC");
+                                        while ($act = mysqli_fetch_array($fetch)) {
+                                        ?>
+                                            <tr>
+                                                <td><?php echo $counter++ . '.'; ?></td>
+                                                <td>
+                                                    <?php 
+                                                    if ($act['m_status'] == '1') {
+                                                        echo "<b>".$act['m_title']."</b>";
+                                                    }else{
+                                                        echo $act['m_title'];
+                                                    }
+                                                     ?>
+                                                </td>
+                                                <td><?php echo $act['m_name']; ?></td>
+                                                <td><?php echo $act['m_email']; ?></td>
+                                                <td><?php echo date("l, d F Y - H:i:s", strtotime($act['m_date'])) ; ?></td>
+                                                <td>
+                                                    <a href="update-article.php?article=<?php echo $act['article_id']; ?>" class="text-primary" title="Update Me"> <i class="fa fa-edit"> </i> </a> |
+                                                    <a href="delete-article.php?article=<?php echo $act['article_id']; ?>" class="text-danger" title="delete Me"> <i class="fa fa-trash"> </i> </a>
+                                                </td>
+                                            </tr>
+                                        <?php } ?>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+                    </div>
+                </div><!--col -->
+            </div><!-- row -->
 
 
         </div><!-- az-content-body -->
