d3bc60b3fc4824b25a35a108a3ea24c48b776ad2
devfaoed <bashirhaadiy@gmail.com>
2023-04-23 15:45:40 +0100

    WTF

---
+++ lovethetruth/admin/pilot.php
@@ -0,0 +1,12 @@
+<?php 
+if (isset($_POST['web'])) {
+    $webname = $_POST['webname'];
+    $web_slug = $_POST['web_slug'];
+    $side_head = $_POST['side_head'];
+    $hero_head = $_POST['hero_head'];
+    $hero_text = $_POST['hero_text'];
+    $side_text = $_POST['side_text'];
+}
+
+
+?>
--- lovethetruth/admin/web-setting.php
+++ lovethetruth/admin/web-setting.php
@@ -77,13 +77,34 @@ include "nav.php";
                                         <textarea name="side_text" class="form-control" id="editor" required><?php echo $web_row['side_text'] ?></textarea>
                                     </div>
                                     <div class="col-md-12 pt-2">
-                                        <button type="submit" name="web1" class="btn btn-primary"><i class="fa fa-sync"></i> Update Information</button>
+                                        <button type="submit" name="web" class="btn btn-primary"><i class="fa fa-sync"></i> Update Information</button>
                                     </div>
                                 </div>
                             </form>
-
                         </div><!-- card-body -->
                     </div><!-- card -->
+
+                    <div class="mg-t-20">
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="card-title">About Informations</div>
+                            </div>
+                            <div class="card-body">
+                                <form method="post">
+                                    <label for="mission">Our Mission</label>
+                                    <textarea name="mission" class="form-control" required><?php echo $web_row['mission'] ?></textarea>
+                                    <label for="vision">Our Vision</label>
+                                    <textarea name="vision" class="form-control" required><?php echo $web_row['vision'] ?></textarea>
+                                    <label for="history">Our History</label>
+                                    <textarea name="history" class="form-control" required><?php echo $web_row['history'] ?></textarea>
+                                    <label for="footer_content">Footer Contents</label>
+                                    <textarea name="hero_head" class="form-control" required><?php echo $web_row['footer_content'] ?></textarea>
+                                    <br>
+                                    <button type="submit" class="btn btn-primary" name="about"><i class="fa fa-sync"></i> Update Contact Information</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div><!-- col -->
                 <div class="col-lg-5 mg-t-20 mg-lg-t-0">
                     <div class="row row-sm">
@@ -108,38 +129,7 @@ include "nav.php";
                             </div><!-- card -->
                         </div><!-- col -->
 
-                        <div class="col-sm-12 mg-t-20 mg-b-20">
-                            <div class="card">
-                                <div class="card-header">
-                                    <p>Contact Information</p>
-                                </div><!-- card-header -->
-                                <div class="card-body">
-                                    <label for="web_email">Website Email</label>
-                                    <input type="text" name="web_email" class="form-control" value="<?php echo $web_row['web_email'] ?>" required>
-                                    <label for="web_phone_1">Phone No.</label>
-                                    <input type="text" name="web_phone_1" class="form-control" value="<?php echo $web_row['web_phone_1'] ?>" >
-                                    <label for="web_phone_2">Tel. Number</label>
-                                    <input type="text" name="web_phone_2" class="form-control" value="<?php echo $web_row['web_phone_2'] ?>" >
-                                    <label for="office_address">Office Address</label>
-                                    <input type="text" name="office_address" class="form-control" value="<?php echo $web_row['office_address'] ?>" >
-                                    <br>
-                                    <p class="border-bottom"><b>Social Media Handles</b></p>
-                                    <label for="facebook">Facebook</label>
-                                    <input type="text" name="facebook" class="form-control" value="<?php echo $web_row['facebook'] ?>" >
-                                    <label for="twitter">Twitter</label>
-                                    <input type="text" name="twitter" class="form-control" value="<?php echo $web_row['twitter'] ?>" >
-                                    <label for="instagram">Instagram</label>
-                                    <input type="text" name="instagram" class="form-control" value="<?php echo $web_row['instagram'] ?>" >
-                                    <label for="whatsapp">WhatsApp</label>
-                                    <input type="text" name="whatsapp" class="form-control" value="<?php echo $web_row['whatsapp'] ?>" >
-                                   <br>
-                                   <button type="submit" class="btn btn-primary"><i class="fa fa-sync"></i> Update Contact Information</button>
-
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-sm-6">
+                        <div class="col-sm-6 mg-t-20">
                             <div class="card ">
                                 <div class="card-header">
                                     <h6>Website Logo Black</h6>
@@ -149,7 +139,7 @@ include "nav.php";
                                 </div><!-- card-body -->
                             </div><!-- card -->
                         </div><!-- col -->
-                        <div class="col-sm-6">
+                        <div class="col-sm-6 mg-t-20">
                             <div class="card ">
                                 <div class="card-header">
                                     <h6>Website Logo Color</h6>
@@ -159,6 +149,51 @@ include "nav.php";
                                 </div><!-- card-body -->
                             </div><!-- card -->
                         </div><!-- col -->
+
+                        <div class="col-sm-12 mg-t-20">
+                            <div class="card ">
+                                <div class="card-header">
+                                    <h6>About Image</h6>
+                                </div><!-- card-header -->
+                                <div class="card-body">
+
+                                </div><!-- card-body -->
+                            </div><!-- card -->
+                        </div><!-- col -->
+
+
+                        <div class="col-sm-12 mg-t-20 mg-b-20">
+                            <div class="card">
+                                <div class="card-header">
+                                    <p>Contact Information</p>
+                                </div><!-- card-header -->
+                                <div class="card-body">
+                                    <label for="web_email">Website Email</label>
+                                    <input type="text" name="web_email" class="form-control" value="<?php echo $web_row['web_email'] ?>" required>
+                                    <label for="web_phone_1">Phone No.</label>
+                                    <input type="text" name="web_phone_1" class="form-control" value="<?php echo $web_row['web_phone_1'] ?>">
+                                    <label for="web_phone_2">Tel. Number</label>
+                                    <input type="text" name="web_phone_2" class="form-control" value="<?php echo $web_row['web_phone_2'] ?>">
+                                    <label for="office_address">Office Address</label>
+                                    <input type="text" name="office_address" class="form-control" value="<?php echo $web_row['office_address'] ?>">
+                                    <br>
+                                    <p class="border-bottom"><b>Social Media Handles</b></p>
+                                    <label for="facebook">Facebook</label>
+                                    <input type="text" name="facebook" class="form-control" value="<?php echo $web_row['facebook'] ?>">
+                                    <label for="twitter">Twitter</label>
+                                    <input type="text" name="twitter" class="form-control" value="<?php echo $web_row['twitter'] ?>">
+                                    <label for="instagram">Instagram</label>
+                                    <input type="text" name="instagram" class="form-control" value="<?php echo $web_row['instagram'] ?>">
+                                    <label for="whatsapp">WhatsApp</label>
+                                    <input type="text" name="whatsapp" class="form-control" value="<?php echo $web_row['whatsapp'] ?>">
+                                    <br>
+                                    <button type="submit" name="contact" class="btn btn-primary"><i class="fa fa-sync"></i> Update Contact Information</button>
+
+                                </div>
+                            </div>
+                        </div>
+
+
 
                     </div><!-- row -->
                 </div><!--col -->
