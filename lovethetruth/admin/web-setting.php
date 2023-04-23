f4cf7fc958fe1afd5e21e37f789bfa0df2f7be49
devfaoed <bashirhaadiy@gmail.com>
2023-04-23 13:21:41 +0100

    up mf

--- lovethetruth/admin/web-setting.php
+++ lovethetruth/admin/web-setting.php
@@ -33,6 +33,85 @@ include "nav.php";
                     <a href="article.php" class="btn btn-purple"><i class="fa fa-plus"></i> Create New Article</a>
                 </div>
             </div><!-- az-dashboard-one-title -->
+            <hr>
+            <div class="az-dashboard-nav">
+                <nav class="nav">
+                    <a class="nav-link active" data-toggle="tab" href="#">Web settings</a>
+                    <a class="nav-link" data-toggle="tab" href="#">Logo and Icons</a>
+                    <a class="nav-link" data-toggle="tab" href="#">Contact Information</a>
+                    <a class="nav-link" data-toggle="tab" href="#">Web Information</a>
+                </nav>
+            </div>
+
+            <div class="row row-sm mg-b-20">
+                <div class="col-lg-7 ht-lg-100p">
+                    <div class="card">
+                        <div class="card-header">
+                            <div>
+                                <h6 class="card-title">Website Information</h6>
+                            </div>
+                        </div><!-- card-header -->
+                        <div class="card-body">
+                            <form method="post" class="form-group">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <label for="webname">Website Name</label>
+                                        <input type="text" name="webname" class="form-control" value="<?php echo $web_row['webname'] ?>" required>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label for="webname">Website Name</label>
+                                        <input type="text" name="webname" class="form-control" value="<?php echo $web_row['webname'] ?>" required>
+                                    </div>
+                                </div>
+                            </form>
+
+                        </div><!-- card-body -->
+                    </div><!-- card -->
+                </div><!-- col -->
+                <div class="col-lg-5 mg-t-20 mg-lg-t-0">
+                    <div class="row row-sm">
+                        <div class="col-sm-6">
+                            <div class="card card-dashboard-two">
+                                <div class="card-header">
+                                    <h6>33.50% <i class="icon ion-md-trending-up tx-success"></i> <small>18.02%</small></h6>
+                                    <p>Bounce Rate</p>
+                                </div><!-- card-header -->
+                                <div class="card-body">
+                                    <div class="chart-wrapper">
+                                        <div id="flotChart1" class="flot-chart"></div>
+                                    </div><!-- chart-wrapper -->
+                                </div><!-- card-body -->
+                            </div><!-- card -->
+                        </div><!-- col -->
+                        <div class="col-sm-6 mg-t-20 mg-sm-t-0">
+                            <div class="card card-dashboard-two">
+                                <div class="card-header">
+                                    <h6>86k <i class="icon ion-md-trending-down tx-danger"></i> <small>0.86%</small></h6>
+                                    <p>Total Users</p>
+                                </div><!-- card-header -->
+                                <div class="card-body">
+                                    <div class="chart-wrapper">
+                                        <div id="flotChart2" class="flot-chart"></div>
+                                    </div><!-- chart-wrapper -->
+                                </div><!-- card-body -->
+                            </div><!-- card -->
+                        </div><!-- col -->
+                        <div class="col-sm-12 mg-t-20">
+                            <div class="card card-dashboard-three">
+                                <div class="card-header">
+                                    <p>All Sessions</p>
+                                    <h6>16,869 <small class="tx-success"><i class="icon ion-md-arrow-up"></i> 2.87%</small></h6>
+                                    <small>The total number of sessions within the date range. It is the period time a user is actively
+                                        engaged with your website, page or app, etc.</small>
+                                </div><!-- card-header -->
+                                <div class="card-body">
+                                    <div class="chart"><canvas id="chartBar5"></canvas></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- row -->
+                </div><!--col -->
+            </div><!-- row -->
 
 
 
